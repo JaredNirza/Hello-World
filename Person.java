9e199dafb95298798f000536f9b7fadc7df70734
JaredNirza <jared.nirza@jmc.edu.ph>
2024-11-05 03:25:49 +0000

    First Program

--- Person.java
+++ Person.java
@@ -3,7 +3,7 @@ class Person{
     int age;
     String gender;
 
-    public static void main(String[] args){
+    public static void main(String[]args){
              System.out.println("Hello World");
              
     
